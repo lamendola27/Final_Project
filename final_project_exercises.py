8be63ddda968f23091cf1bfd84799c14251e2a4a
carli2133 <74063675+carli2133@users.noreply.github.com>
2020-12-02 21:38:02 -0500

    rough draft Exercise 5  added
    I added my work on exercise 5. Was only able to plot the y = x line successfully

--- final_project_exercises.py
+++ final_project_exercises.py
@@ -22,3 +22,36 @@ def tentmap(y, n):
  tentmap(0.34685264,3)
 
 
+#Exercise 5 Very rough draft 
+import numpy as np
+import matplotlib.pyplot as plt 
+
+def cobweb(f, x0, n, xmin, xmax, ymin, ymax):
+	x = x0
+	ynext = f(x)
+	X = []
+	Y = []
+	for i in range(0, n, 2):
+		xnew = ynext
+		xold = x 
+		x = xnew
+		ynext = f(x)
+		X.append(xold)
+		X.append(x) 
+		X.append(x)
+		Y.append(xnew) 
+		Y.append(xnew) 
+		Y.append(ynext)
+	plt.figure()
+	plt.plot(X, Y)
+	plt.plot(x, f(x))
+
+	diagnolline = []
+	for i in range(0,100):
+		diagnolline.append(i)
+
+	plt.plot(diagnolline)
+	plt.legend()
+	plt.show()
+
+print(cobweb(np.cos, 1.0, 200, 0, 1.5, 0, 1))
