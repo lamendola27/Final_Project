8a8bb66e160850ec6d19005470e7c477456c79b1
carli2133 <74063675+carli2133@users.noreply.github.com>
2020-12-10 12:37:17 -0500

    Exercise 5 all three functions on 1 graph
    Exercise 5 fully running

--- exercise5.py
+++ exercise5.py
@@ -1,9 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt 
-
-diagnolline = []
-for i in range(0,100):
-    diagnolline.append(i)
     #plt.plot(diagnolline)
 def cobweb(f, x0, n, xmin, xmax, ymin, ymax):
 	x = x0
@@ -21,11 +17,30 @@ def cobweb(f, x0, n, xmin, xmax, ymin, ymax):
 		Y.append(xnew) 
 		Y.append(xnew) 
 		Y.append(ynext)
+
+	k = np.linspace(0, xmax, n+1)
+	y = f(k)
+	i = np.linspace(0, xmax, n+1)
 	plt.figure()
-	plt.plot(X, Y)
-	plt.plot(f(x))
-    plt.plot(diagonalline)
-plt.legend()
-plt.show()
+	plt.plot(X, Y, label='cobweb')
+	plt.plot(i, i, label='y=x')
+	plt.plot(k, y, label='f(x)')
+	plt.ylim([0, ymax])
+	plt.legend()
+	plt.show()
 
 print(cobweb(np.cos, 1.0, 200, 0, 1.5, 0, 1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
